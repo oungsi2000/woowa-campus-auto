34cfa2555d774ffae8f64b451a70112b2822f8c7
oungsi2000 <oungsi1000@gmail.com>
2025-06-23 02:31:50 +0900

    refactor: Compose View 리팩터링  - 뷰모델 인자로 받기  - string, color 분리  - 별도의 View 파일로 분리

---
+++ app/src/main/java/com/example/campus_auto/MainView.kt
@@ -0,0 +1,227 @@
+package com.example.campus_auto
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.campus_auto.ui.theme.CampusautoTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.campus_auto.ui.theme.Primary
+import com.example.campus_auto.ui.theme.PrimaryDanger
+import com.example.campus_auto.ui.theme.Secondary
+import com.example.campus_auto.ui.theme.SecondaryDanger
+
+
+@Preview(showBackground = true)
+@Composable
+fun MainView(viewModel: MainViewModel = viewModel()) {
+    CampusautoTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopBar()
+            },
+            bottomBar = {
+                BottomNetWorkBanner()
+            }
+        ) { innerPadding ->
+            MainBackground()
+            AutoCheckInStart(
+                modifier = Modifier
+                    .padding(innerPadding)
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(stringResource(R.string.topbar_text))
+            }
+        }
+    )
+}
+
+@Composable
+fun AutoCheckInStart(modifier:Modifier) {
+    var isEnabled by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .offset(y = (-100).dp)
+        ,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Black,
+            modifier = Modifier.padding(16.dp),
+            text = stringResource(R.string.description_auto_check),
+        )
+
+        if (!isEnabled) {
+            AutoCheckButtonEnabled {
+                isEnabled = true
+            }
+        } else {
+            AutoCheckButtonDisabled {
+                isEnabled = false
+            }
+        }
+    }
+}
+
+@Composable
+fun AutoCheckButtonEnabled(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .width(260.dp)
+            .height(48.dp),
+        enabled = true,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.PrimaryDanger,
+            contentColor = Color.White
+        )
+    ) {
+        Text(
+            fontSize = 14.sp,
+            text = stringResource(R.string.auto_check_start)
+        )
+    }
+}
+
+@Composable
+fun AutoCheckButtonDisabled(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .width(260.dp)
+            .height(48.dp),
+        enabled = true,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Primary,
+            contentColor = Color.White
+        )
+    ) {
+        Text(
+            fontSize = 14.sp,
+            text = stringResource(R.string.auto_check_stop)
+        )
+    }
+}
+
+
+
+@Composable
+fun MainBackground() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.haengseoungee),
+            contentDescription = "background",
+            modifier = Modifier
+                .scale(2f)
+                .offset(x = 100.dp, y = 300.dp)
+            ,
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Composable
+fun BottomNetWorkBanner() {
+    var isEnabled by remember { mutableStateOf(false) }
+
+    if (isEnabled) {
+        BottomNetWorkEnabled()
+    } else {
+        BottomNetWorkDisabled()
+    }
+}
+
+@Composable
+fun BottomNetWorkEnabled() {
+    BottomAppBar(
+        containerColor = Color.Secondary,
+        modifier = Modifier.height(72.dp)
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.tool_current_ip),
+                color = Color.Black,
+            )
+            Text(
+                text = stringResource(R.string.can_check_in),
+                color = Color.Black,
+            )
+        }
+    }
+}
+
+@Composable
+fun BottomNetWorkDisabled() {
+    BottomAppBar(
+        containerColor = Color.SecondaryDanger,
+        modifier = Modifier.height(72.dp)
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.tool_current_ip),
+                color = Color.Black,
+            )
+            Text(
+                text = stringResource(R.string.cannot_check_in),
+                color = Color.Black,
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/campus_auto/ui/theme/Color.kt
+++ app/src/main/java/com/example/campus_auto/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Color.Companion.Primary: Color
+    get() = Color(0xFFDF3E62)
+val Color.Companion.Secondary: Color
+    get() = Color(0xFFA6DAF4)
+val Color.Companion.PrimaryDanger: Color
+    get() = Color(0xFF00AFF0)
+val Color.Companion.SecondaryDanger: Color
+    get() = Color(0xFFF27B92)
+
